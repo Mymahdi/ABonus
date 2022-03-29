e42e79632df2acfe208814b7c562482fbb274349
Mymahdi <henry15ronaldo79carlo1@gmail.com>
2022-03-29 05:06:40 -0400

    devid function 'solve' to many finctions

--- solution.cpp
+++ solution.cpp
@@ -4,38 +4,23 @@
 using namespace std;
 
 
-//----------------
-long int sum_calculate(vector<int> v){
-     long int sum = 0;
-    for (int i = 0; i < v.size(); i++)
-    sum += v[i];
-    return sum;
-}
-//--------------------------------
-void solve(int m, int n, long int& harvest , vector<vector<int> > &farm){
-    long int sum = 0;
-    int max_row = 0;
-    int max_column = 0;
-    int row = 0;
-    int column = 0;
 
-    for(int move = 0; move < 4; move++) {
-        vector<long int> row_sums;
+void find_max_row(int m ,int &row ,int &max_row, vector<long int> row_sums){
 
-        for(int i = 0; i < m; i++){
-            sum = sum_calculate(farm[i]);
-            row_sums.push_back(sum);
-            sum = 0;
-        }
-
-        for(int i = 0; i < m; i++){
+     for(int i = 0; i < m; i++){
             if (row_sums[i] > max_row) {
                 max_row = row_sums[i];
                 row = i;
             }
         }
+}
 
-        for(int j = 0; j < n; j++){
+
+void find_max_column(int m ,int n ,int &column ,int &max_column , vector<vector<int> > farm){
+
+    int sum = 0;
+
+    for(int j = 0; j < n; j++){
             for(int i = 0; i < m; i++){
                 sum += farm[i][j];
             }
@@ -45,7 +30,28 @@ void solve(int m, int n, long int& harvest , vector<vector<int> > &farm){
             }
             sum = 0;
         }
-        if(max_column > max_row){
+}
+//----------------
+long int sum_calculate(vector<int> v){
+     long int sum = 0;
+    for (int i = 0; i < v.size(); i++)
+    sum += v[i];
+    return sum;
+}
+
+void built_row_sums(int m , vector<long int> &row_sums,vector<vector<int> > farm){
+    int sum = 0;
+       for(int i = 0; i < m; i++){
+            sum = sum_calculate(farm[i]);
+            row_sums.push_back(sum);
+            sum = 0;
+        }
+}
+
+void calculate_harvest(long int &harvest , int &column , int &row ,int m ,int n , int max_column ,int max_row,
+    vector<vector<int> > &farm){
+
+     if(max_column > max_row){
             harvest += max_column;
             for(int i = 0; i < m; i++){
                 farm[i][column] = 0;
@@ -57,6 +63,24 @@ void solve(int m, int n, long int& harvest , vector<vector<int> > &farm){
                 farm[row][j] = 0;
             }
         }
+}
+//--------------------------------
+void solve(int m, int n, long int& harvest , vector<vector<int> > &farm){
+    long int sum = 0;
+    int max_row = 0;
+    int max_column = 0;
+    int row = 0;
+    int column = 0;
+
+    for(int move = 0; move < 4; move++) {
+        vector<long int> row_sums;
+        built_row_sums( m , row_sums ,farm);
+     
+        find_max_row(m ,row ,max_row ,row_sums);
+       
+        find_max_column(m ,n ,column ,max_column ,farm);
+        
+        calculate_harvest(harvest ,column ,row , m , n , max_column ,max_row,farm);
         row = 0;
         column = 0;
         max_column = 0;
